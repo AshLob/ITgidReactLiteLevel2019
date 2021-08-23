56de66d4c3e5470f178a036414b61e4882a4f470
Владимир <intra@outlook.com>
2021-08-23 17:41:54 +0300

    Сделано как в уроке.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contacts from './Contacts';
+import Other from './Other';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,17 +15,22 @@ class App extends React.Component {
   }
   render(){
     return (
-      <div className="App">
-        <ul>
-          <li><a href="/">Main page</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contacts">Contacts</a></li>                    
-        </ul>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
+      <Router>
+        <div className="App">
+          <ul>
+            <li><a href="/">Main page</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contacts">Contacts</a></li>                    
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route component={Other} />         
+          </Switch>
 
-      </div>
+        </div>
+      </Router>
     );
     }
 }
---
+++ src/Other.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+//import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+class Other extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "lesson": "Lesson 14"
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>404</h1>
+       {/*<ul>
+          <li><a href="/">Main page</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contacts">Contacts</a></li>                    
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+       </Switch>*/}
+
+      </div>
+    );
+    }
+}
+
+export default Other;
